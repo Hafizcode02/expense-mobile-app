d4340519e21824af604ad6b3a7362d87e4569448
Hafiz Caniago <hafizcode02@gmail.com>
2023-07-12 19:25:30 +0700

    finish layout Login and Register

--- app/src/main/java/hafizcaniago/my/id/papb_final/LoginActivity.java
+++ app/src/main/java/hafizcaniago/my/id/papb_final/LoginActivity.java
@@ -1,14 +1,24 @@
 package hafizcaniago.my.id.papb_final;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button btnToRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnToRegister = findViewById(R.id.btnRegister);
+        btnToRegister.setOnClickListener(view -> {
+            Intent moveActivity = new Intent(getApplicationContext(), RegisterActivity.class);
+            startActivity(moveActivity);
+        });
     }
 }
--- app/src/main/java/hafizcaniago/my/id/papb_final/RegisterActivity.java
+++ app/src/main/java/hafizcaniago/my/id/papb_final/RegisterActivity.java
@@ -1,14 +1,27 @@
 package hafizcaniago.my.id.papb_final;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    Button btnToLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnToLogin = findViewById(R.id.btnLogin);
+        btnToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent moveActivity = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(moveActivity);
+            }
+        });
     }
 }
