987fd19980fd18efff8ef32083f38693f4bb8fb9
Hafiz Caniago <hafizcode02@gmail.com>
2023-07-12 18:52:13 +0700

    add file that want to use and also finish login view

---
+++ app/src/main/java/hafizcaniago/my/id/papb_final/RegisterActivity.java
@@ -0,0 +1,14 @@
+package hafizcaniago.my.id.papb_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
