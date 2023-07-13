3876f6e4fa3a49963a9022ce761c8fc5bb9caffa
Hafiz Caniago <hafizcode02@gmail.com>
2023-07-13 17:14:49 +0700

    Profile Activity Layout

--- app/src/main/java/hafizcaniago/my/id/papb_final/ProfileActivity.java
+++ app/src/main/java/hafizcaniago/my/id/papb_final/ProfileActivity.java
@@ -1,14 +1,32 @@
 package hafizcaniago.my.id.papb_final;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class ProfileActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        setupGenderAutoComplete();
+    }
+
+    public void setupGenderAutoComplete() {
+        String[] genderList = new String[]{"Male", "Female", "Prefer Not to Say"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                R.layout.drop_down_item,
+                genderList
+        );
+
+        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.txtGender);
+        autoCompleteTextView.setAdapter(adapter);
+
+        autoCompleteTextView.setOnItemClickListener((adapterView, view, i, l) -> Toast.makeText(getApplicationContext(), adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show());
     }
 }
