ca063ded6e9479741c7dbf1993a0e23d0ea6f7c9
Hafiz Caniago <hafizcode02@gmail.com>
2023-07-13 20:28:10 +0700

    Add DatePicker to Profile Detail

--- app/src/main/java/hafizcaniago/my/id/papb_final/ProfileActivity.java
+++ app/src/main/java/hafizcaniago/my/id/papb_final/ProfileActivity.java
@@ -3,17 +3,26 @@ package hafizcaniago.my.id.papb_final;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.textfield.TextInputEditText;
+
 public class ProfileActivity extends AppCompatActivity {
+    AutoCompleteTextView autoCompleteTextView;
+    TextInputEditText dateEditText;
+    Button showDateToSelect;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
         setupGenderAutoComplete();
+        setupDateButton();
     }
 
     public void setupGenderAutoComplete() {
@@ -24,9 +33,23 @@ public class ProfileActivity extends AppCompatActivity {
                 genderList
         );
 
-        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.txtGender);
+        autoCompleteTextView = findViewById(R.id.txtGender);
         autoCompleteTextView.setAdapter(adapter);
 
         autoCompleteTextView.setOnItemClickListener((adapterView, view, i, l) -> Toast.makeText(getApplicationContext(), adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show());
+    }
+
+    public void setupDateButton() {
+        dateEditText = findViewById(R.id.edtDateText);
+        showDateToSelect = findViewById(R.id.btnShowDate);
+
+        MaterialDatePicker datePicker = MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select Date Of Birth").setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .build();
+
+        showDateToSelect.setOnClickListener(view -> {
+            datePicker.show(getSupportFragmentManager(), "Material_Date_Picker");
+            datePicker.addOnPositiveButtonClickListener(selection -> dateEditText.setText(datePicker.getHeaderText()));
+        });
     }
 }
