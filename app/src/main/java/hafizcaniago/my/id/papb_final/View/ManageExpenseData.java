62626fc5132008aedb5aafd1ffb9818d5b8db103
Hafiz Caniago <hafizcode02@gmail.com>
2023-07-15 06:19:56 +0700

    Create ManageExpenseData and Finish Base Layout

---
+++ app/src/main/java/hafizcaniago/my/id/papb_final/View/ManageExpenseData.java
@@ -0,0 +1,16 @@
+package hafizcaniago.my.id.papb_final.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import hafizcaniago.my.id.papb_final.R;
+
+public class ManageExpenseData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_expense_data);
+    }
+}
